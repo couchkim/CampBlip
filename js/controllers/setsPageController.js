4a75f3ca8617e098d73e1b25c743ecd3ed8d1fc5
Kimberly Hanson <kimberlyhanson@KimAir13.local>
2017-02-07 09:24:49 -0500

    added Set constructor

--- js/controllers/setsPageController.js
+++ js/controllers/setsPageController.js
@@ -8,14 +8,21 @@ module.exports = {
         $scope.available = ['Available', 'Checked Out', 'Missing Pieces', 'On Display'];
 
         $scope.themes = ['Advanced Models', 'Agents', 'Alien Conquest', 'Aqua Raiders', 'Architecture', 'Atlantis', 'Batman', 'Bionicle', 'Cars2', 'Castle',
-                        'Chima', 'City', 'Creator', 'Dino', 'Friends', 'Galaxy Squad', 'Ghostbusters', 'Harry Potter', 'Hero Factory', 'Hobbit', 'Indiana Jones', 'Lego Movie', 
-                        'Lone Ranger', 'Lord of the Rings', 'Mars Mission', 'Minecraft', 'Monster Fighters' , 'Ninjago', 'Power Miners', 'Prince of Persia', 'Racers', 'Scooby Doo', 'Space Police',
-                        'Speed Champions', 'Spongebob', 'Sports', 'Star Wars', 'Super Heroes', 'Technic', 'Teenage Mutant Ninja Turtles',
-                        'The Simpsons', 'Toy Story', 'Ultra Agents', 'Vintage', 'Wall-E']
+            'Chima', 'City', 'Creator', 'Dino', 'Friends', 'Galaxy Squad', 'Ghostbusters', 'Harry Potter', 'Hero Factory', 'Hobbit', 'Indiana Jones', 'Lego Movie',
+            'Lone Ranger', 'Lord of the Rings', 'Mars Mission', 'Minecraft', 'Monster Fighters', 'Ninjago', 'Power Miners', 'Prince of Persia', 'Racers', 'Scooby Doo', 'Space Police',
+            'Speed Champions', 'Spongebob', 'Sports', 'Star Wars', 'Super Heroes', 'Technic', 'Teenage Mutant Ninja Turtles',
+            'The Simpsons', 'Toy Story', 'Ultra Agents', 'Vintage', 'Wall-E'];
 
-         console.log('sets page controller working');
-            
-        
+        $scope.viewSets = function () {
+            CampService.getSets();
+            console.log(sets);
+        };
+
+
+
+        console.log('sets page controller working');
+
+
 
     },
 }
--- js/services/CampService.js
+++ js/services/CampService.js
@@ -4,15 +4,36 @@ module.exports = {
     func: function () {
 
 
+function Set(name, number, theme, pieces, status, image) {
+            this.title = name;
+            this.setNumber = number;
+            this.theme = theme;
+            this.pieces = pieces;
+            this.status = status;
+            this.image = image;
+
+
+            return this;
+        }
+
+
         let sets = [];
 
-        return {
+        sets.push(new Set('Space Moon Buggy', '3365-1', 'City', 37, 'Available', 'http://images.brickset.com/sets/images/3365-1.jpg?201012090229'));
+        sets.push(new Set('The First Night', '21115-1', 'Minecraft', 408, 'Checked Out', 'http://images.brickset.com/sets/large/21115-1.jpg?201410080618'));
+        sets.push(new Set('The Batcave','6860-1', 'Super Heroes', 690, 'Missing Pieces', 'http://images.brickset.com/sets/images/6860-1.jpg?201110150701'));
+        sets.push(new Set('Death Star','10188-1', 'Star Wars', 3803, 'On Display', 'http://images.brickset.com/sets/AdditionalImages/10188-1/10188-0000-xx-13-1.jpg'));
 
+        console.log(sets);
+
+        return {
 
             getSets() {
 
                 return sets;
             },
+
+
 
         }
 
