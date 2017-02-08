6892f1feea49d471af201784a59265946b800eb2
Kimberly Hanson <kimberlyhanson@KimAir13.local>
2017-02-08 09:59:29 -0500

    added all data to details page

--- js/services/CampService.js
+++ js/services/CampService.js
@@ -4,13 +4,14 @@ module.exports = {
     func: function ($http) {
 
 
-        function Set(name, number, theme, pieces, status, image) {
+        function Set(name, number, theme, pieces, status, image, year) {
             this.setName = name;
             this.setNumber = number;
             this.theme = theme;
             this.pieces = pieces;
             this.status = status;
             this.image = image;
+            this.year = year;
 
 
             return this;
@@ -19,11 +20,11 @@ module.exports = {
 
         let sets = [];
 
-        sets.push(new Set('Space Moon Buggy', '3365-1', 'City', 37, 'Available', 'http://images.brickset.com/sets/images/3365-1.jpg?201012090229'));
-        sets.push(new Set('The First Night', '21115-1', 'Minecraft', 408, 'Checked Out', 'http://images.brickset.com/sets/large/21115-1.jpg?201410080618'));
-        sets.push(new Set('The Batcave', '6860-1', 'Super Heroes', 690, 'Missing Pieces', 'http://images.brickset.com/sets/images/6860-1.jpg?201110150701'));
-        sets.push(new Set('Death Star', '10188-1', 'Star Wars', 3803, 'On Display', 'http://images.brickset.com/sets/AdditionalImages/10188-1/10188-0000-xx-13-1.jpg'));
-        sets.push(new Set('Yellow Submarine', '21306-1', 'Ideas', 553, 'Checked Out', 'http://images.brickset.com/sets/images/21306-1.jpg?201610150938'));
+        sets.push(new Set('Space Moon Buggy', '3365-1', 'City', 37, 'Available', 'http://images.brickset.com/sets/images/3365-1.jpg?201012090229', 2005));
+        sets.push(new Set('The First Night', '21115-1', 'Minecraft', 408, 'Checked Out', 'http://images.brickset.com/sets/large/21115-1.jpg?201410080618', 2014));
+        sets.push(new Set('The Batcave', '6860-1', 'Super Heroes', 690, 'Missing Pieces', 'http://images.brickset.com/sets/images/6860-1.jpg?201110150701', 2013));
+        sets.push(new Set('Death Star', '10188-1', 'Star Wars', 3803, 'On Display', 'http://images.brickset.com/sets/AdditionalImages/10188-1/10188-0000-xx-13-1.jpg', 2015));
+        sets.push(new Set('Yellow Submarine', '21306-1', 'Ideas', 553, 'Checked Out', 'http://images.brickset.com/sets/images/21306-1.jpg?201610150938', 2017));
 
         console.log(sets);
 
