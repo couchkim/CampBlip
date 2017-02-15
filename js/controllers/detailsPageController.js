ca050bfae9da154a4b721086c4093c4197326e87
Kimberly Hanson <kimberlyhanson@KimAir13.local>
2017-02-15 09:42:08 -0500

    added camper list

--- js/controllers/detailsPageController.js
+++ js/controllers/detailsPageController.js
@@ -3,18 +3,24 @@ module.exports = {
     name: "detailsPageController",
     func: function ($scope, CampService, $stateParams) {
 
-       const id = ($stateParams.single);
-       
-       $scope.item = CampService.getSet(id);
-       console.log($scope.item);
+
+        $scope.campers = ['Wilson Rhee', 'Carter Bateman', 'Collier McElroy',
+            'Alexander Paschal', 'Holden Harrell'];
+
+        $scope.camperList = '';
+
+        const id = ($stateParams.single);
+
+        $scope.item = CampService.getSet(id);
+        console.log($scope.item);
 
 
-       $scope.getParts = function(item){
-           CampService.showParts(item);
-           console.log(CampService.showParts(item));
-       }
-            // console.log('detail page controller working');
-        $scope.checkToggle = function(){
+        $scope.getParts = function (item) {
+            CampService.showParts(item);
+            console.log(CampService.showParts(item));
+        }
+        // console.log('detail page controller working');
+        $scope.checkToggle = function () {
             $scope.item.status = CampService.changeStatus($scope.item.status);
         }
 
